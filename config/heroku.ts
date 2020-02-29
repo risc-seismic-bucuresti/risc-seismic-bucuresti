5cfc301701c2f6d6076b47c2114d50a7ab6a06a2
Alexandru Neamtu <alexneamtu@gmail.com>
2020-02-29 23:15:06 +0200

    Updates for heroku

---
+++ config/heroku.ts
@@ -0,0 +1,18 @@
+export const config = {
+  web: {
+    port: 3040,
+  },
+  db: {
+    host: 'ec2-54-247-125-38.eu-west-1.compute.amazonaws.com',
+    username: 'zcnzsjdjhpngyh',
+    password: '097638bfcaa0791f05441b24f76548902e374c77720ba64876a1e6ab51bf0895',
+    database: 'demud3s0gq1k0t',
+    manageDatabase: false,
+    ssl: true,
+  },
+  cache: {
+    redis: {
+      url: 'redis://h:p4aaaa8fa91742dcfef6e55a3d8a7e91afb919714ae03ff8f6c1c232335208b23@ec2-18-200-126-4.eu-west-1.compute.amazonaws.com:9059',
+    },
+  },
+}
