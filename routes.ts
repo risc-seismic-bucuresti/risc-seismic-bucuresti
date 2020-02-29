3403e66d62f9a7ac9d4364bb756fd5c094c6b2bd
Alexandru Neamtu <alexneamtu@gmail.com>
2020-02-29 23:26:15 +0200

    Updates for heroku

--- routes.ts
+++ routes.ts
@@ -6,6 +6,14 @@ import { Op } from 'sequelize';
 // models
 import { Building, BuildingRating } from "./models";
 
+// services
+import { CacheService } from "./services";
+
+// config
+import { config } from "./config";
+
+const cacheService = CacheService.getInstance(config.cache.redis)
+
 export class Routes extends KoaRouter {
   constructor() {
     super();
@@ -14,17 +22,22 @@ export class Routes extends KoaRouter {
       try {
         const address = _.get(ctx, 'request.query.address')
         const number = _.get(ctx, 'request.query.number')
-        const buildings = await Building.findAll({
-          where: {
-            address: { [Op.iLike]: `%${address}%` },
-            addressNumber: { [Op.iLike]: `%${number}%` },
-          },
-          include: [{
-            model: BuildingRating,
-          }],
-        });
+        let data = await cacheService.getCache(`${address}-${number}`);
+        if (!data) {
+          const buildings = await Building.findAll({
+            where: {
+              address: { [Op.iLike]: `%${address}%` },
+              addressNumber: { [Op.iLike]: `%${number}%` },
+            },
+            include: [{
+              model: BuildingRating,
+            }],
+          });
+          await cacheService.setCache(`${address}-${number}`, JSON.stringify(buildings))
+          data = buildings
+        }
         ctx.status = 200;
-        ctx.body = buildings;
+        ctx.body = data;
       } catch (err) {
         ctx.throw(400, err.message);
       }
