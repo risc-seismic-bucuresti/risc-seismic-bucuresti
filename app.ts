d93d77984682a86678d9514b9d04cd4a1eb39bc1
Alexandru Neamtu <alexneamtu@gmail.com>
2020-02-29 23:26:15 +0200

    Updates for heroku

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 // services
-import { DbService, WebService } from './services/';
+import { CacheService, DbService, WebService } from './services/';
 
 // routers
 import { Routes } from './routes';
@@ -9,10 +9,12 @@ import { config } from './config';
 
 const routes = new Routes();
 
+const cacheService = CacheService.getInstance(config.cache.redis)
 const dbService = new DbService(config.db);
 const webServer = new WebService({ web: config.web }, [routes]);
 
 (async () => {
+  await cacheService.initialize();
   await dbService.initialize();
   await webServer.listen();
 })();
