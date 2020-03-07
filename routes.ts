8348ad7089f5e0ff7f7ed85e85c82ba8ebfb82ed
Alex Neamtu <alexneamtu@gmail.com>
2020-03-07 14:21:32 +0200

    - Allow input diacritics - Fix multi string addres search

--- routes.ts
+++ routes.ts
@@ -12,18 +12,22 @@ import { CacheService } from "./services";
 // config
 import { config } from "./config";
 
-const cacheService = CacheService.getInstance(config.cache.redis)
+const cacheService = CacheService.getInstance(config.cache.redis);
 
 export class Routes extends KoaRouter {
+  private static cleanDiacritics(input: string): string {
+    if (!input) return input;
+    return input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
   constructor() {
     super();
 
     this.get('/', async (ctx) => {
       try {
-        const address = _.get(ctx, 'request.query.address')
-        const number = _.get(ctx, 'request.query.number')
+        const address = Routes.cleanDiacritics(_.get(ctx, 'request.query.address'));
+        const number = Routes.cleanDiacritics(_.get(ctx, 'request.query.number'));
 
-        const ip = ctx.request.headers['x-client-ip'] || ctx.request.ip
+        const ip = ctx.request.headers['x-client-ip'] || ctx.request.ip;
         const ipCalls = await cacheService.getCache(ip);
 
         if (!ipCalls) {
@@ -36,16 +40,18 @@ export class Routes extends KoaRouter {
         let data = await cacheService.getCache(`${address}-${number}`);
         if (!data) {
           await cacheService.incrCache('db-calls');
+          const addressArray = address.split(/\W/);
+          const addressQuery = _.map(addressArray, (addressElement) => ({ [Op.iLike]: `%${addressElement}%` }));
           const buildings = await Building.findAll({
             where: {
-              address: { [Op.iLike]: `%${address}%` },
+              address: { [Op.or]: addressQuery },
               addressNumber: { [Op.iLike]: `%${number}%` },
             },
             include: [{
               model: BuildingRating,
             }],
           });
-          await cacheService.setCache(`${address}-${number}`, JSON.stringify(buildings))
+          await cacheService.setCache(`${address}-${number}`, JSON.stringify(buildings));
           data = buildings
         }
         ctx.status = 200;
